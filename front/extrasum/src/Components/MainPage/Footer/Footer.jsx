368346b8f95fd72e41a454078083b662e41eeda5
Raoufdine SAID <said.raoufdine@gmail.com>
2023-08-02 22:57:42 +0200

    Footer ready

---
+++ front/extrasum/src/Components/MainPage/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Footer = () => {
+
+    return (
+        <div
+            className="w-full h-16 bg-gray-700 text-white flex justify-center items-center text-center"
+        >
+            &copy; {(new Date()).getFullYear()} - Raoufdine SAID
+        </div>
+    );
+};
+
+export default Footer;
