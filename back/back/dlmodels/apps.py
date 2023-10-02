cab56e44fa628af762a56381650b75618d54956f
Raoufdine SAID <said.raoufdine@gmail.com>
2023-10-02 16:58:44 +0200

    updating canceled

--- back/back/dlmodels/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExtrasumModelConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'extrasum_model'
--- back/back/dlmodels/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import sum_api_view
-
-urlpatterns = [
-    path('models/', sum_api_view, name='summodels')
-]
